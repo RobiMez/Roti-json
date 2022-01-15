48c928223af6063d99ccd62093cfb4fc4af19aa1
Robi <53259730+RobiMez@users.noreply.github.com>
2022-01-15 14:43:04 +0300

    fixes #3
    also corrected filename and added a link to github on start command

--- ptbot.py
+++ ptbot.py
@@ -1,9 +1,13 @@
 import json
 import random
-
+import os
+import dotenv
 from telegram.ext import Updater
-updater = Updater(token='2037773235:AAFuGYes7IRVFKHGMOe_Nhz7yeyIi2VHLj4', use_context=True)
 
+dotenv.load_dotenv('.env')
+TOKEN = os.getenv('TOKEN')
+
+updater = Updater(token=TOKEN, use_context=True)
 dispatcher = updater.dispatcher
 
 import logging
@@ -11,10 +15,10 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
                      level=logging.INFO)
 
 def start(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi there , Im random Roti ( Rules Of The Internet ) bot , send /rroti to get a random rule .\n\n Made by Robi")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hi there , Im random Roti ( Rules Of The Internet ) bot , send /rroti to get a random rule .\n\n Made by github.com/robimez")
     
 def rroti(update, context):
-    with open('./roti.json', encoding="utf8") as f:
+    with open('./rules_of_the_internet.json', encoding="utf8") as f:
         data = json.load(f)
         context.bot.send_message(chat_id=update.effective_chat.id, text=f"Rule {random.choice(list(data))} : {data[random.choice(list(data))]}")
 
@@ -25,7 +29,7 @@ dispatcher.add_handler(start_handler)
 start_handler = CommandHandler('rroti', rroti)
 dispatcher.add_handler(start_handler)
 
-
+print('Bot started.')
 updater.start_polling()
 
 
